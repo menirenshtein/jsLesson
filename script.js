b32fc292a242c2dc90396a7c05592808180afadb
menirenshtein <m4584a@gmail.com>
2024-09-04 17:16:24 +0300

    nothing is done

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
-let a = "a";
-let b = "zzzzzzzzzzz"
+// const people = ["Greg", "Mary", "Devon", "James"];
+
+// let newArr = people
+// newArr.push = "bob"
+// console.log(newArr);
 
 
-let fullName = "meni iren"
+// let index = people.indexOf("Greg")
 
-let space = fullName.indexOf("")
-let first = fullName.substring(0,space)
-let last = fullName.substring(space,)
-console.log(space, first, last);
+// people.splice(index ,1, "avi")
+// people.pop();
+// people.unshift("Matt")
+// let newArr = [];
+// newArr.push(people[people.indexOf("")] )
 
+
+// for (let index = 0; index < people.length; index++) {
+//         if (people[index] !== "Matt" && people[index] !== "Mary")
+//         {
+//             newArr.push(people[index])
+//         }
+    
+    
+    // }
+    // console.log(newArr);
+    
+    
+    // let indexOf = people.indexOf("Mary")
+    // console.log(indexOf);
+    
+    
+    // function changeNamesArray(arr, nametoDelete, nameToAdd){
+    //     try {
+    //         let index = arr.indexOf(nametoDelete)
+    //         if (index >= 0) {
+    //             arr.splice(index,1,nameToAdd)
+    //             console.log(people);
+    //         }
+            
+    //     } catch (error) {
+    //         throw new Error("no such name");
+            
+    //     }
+    //     console.log(people, newArr);
+         
+    // }
+    // changeNamesArray(people, "Matt", "shmuel")
+    // console.log(changeNamesArray(people, "Matt", "shmuel"));
+    
+    
+    
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+    // fruits.splice(2,0,"Lemon", "Kiwi" )
+    // console.log(fruits);
+    
+    // const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+    // let a = fruits.slice(1,3)
+    // fruits.splice(0,1)
+
+    // console.log(a);
+    
+
+    const book = {
+        title: String,
+
+    }
+    let b = (a)=> a * 5
+    console.log(b(5));
+    
