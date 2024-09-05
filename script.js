67a823bf685003c4a45937addfd12947c918858b
menirenshtein <m4584a@gmail.com>
2024-09-05 09:23:01 +0300

    hiii

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const people = ["Greg", "Mary", "Devon", "James"];
+const people = ["Greg", "Mary", "Devon", "James"];
 
 // let newArr = people
 // newArr.push = "bob"
@@ -19,52 +19,60 @@
 //         {
 //             newArr.push(people[index])
 //         }
-    
-    
-    // }
-    // console.log(newArr);
-    
-    
-    // let indexOf = people.indexOf("Mary")
-    // console.log(indexOf);
-    
-    
-    // function changeNamesArray(arr, nametoDelete, nameToAdd){
-    //     try {
-    //         let index = arr.indexOf(nametoDelete)
-    //         if (index >= 0) {
-    //             arr.splice(index,1,nameToAdd)
-    //             console.log(people);
-    //         }
-            
-    //     } catch (error) {
-    //         throw new Error("no such name");
-            
-    //     }
-    //     console.log(people, newArr);
-         
-    // }
-    // changeNamesArray(people, "Matt", "shmuel")
-    // console.log(changeNamesArray(people, "Matt", "shmuel"));
-    
-    
-    
-    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-    // fruits.splice(2,0,"Lemon", "Kiwi" )
-    // console.log(fruits);
-    
-    // const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-    // let a = fruits.slice(1,3)
-    // fruits.splice(0,1)
 
-    // console.log(a);
-    
+// }
+// console.log(newArr);
 
-    const book = {
-        title: String,
 
-    }
-    let b = (a)=> a * 5
-    console.log(b(5));
+// let indexOf = people.indexOf("Mary")
+// console.log(indexOf);
+
+
+// function changeNamesArray(arr, nametoDelete, nameToAdd){
+//     try {
+//         let index = arr.indexOf(nametoDelete)
+//         if (index >= 0) {
+//             arr.splice(index,1,nameToAdd)
+//             console.log(people);
+//         }
+        
+//     } catch (error) {
+//         throw new Error("no such name");
+        
+//     }
+//     console.log(people, newArr);
+        
+// }
+// changeNamesArray(people, "Matt", "shmuel")
+// console.log(changeNamesArray(people, "Matt", "shmuel"));
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits.splice(2,0,"Lemon", "Kiwi" )
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// let a = fruits.slice(1,3)
+// fruits.splice(0,1)
+
+// console.log(a);
+
+
+const book = {
+    title: String,
+
+}
+let b = (a)=> a * 5
+// console.log(b(5));
+
+let arr = [1, 2, 3 , 4, 5, ]
+
+arr.forEach((val, ) => {
     
+});
+
+let a = arr.filter((val) => {return val >= 4})
+console.log(a)
